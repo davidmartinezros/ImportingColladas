6e07d31fced2de5a1eaa9af9f804bd304b93fab7
David Martínez Ros <David Martnez Ros>
2016-10-29 19:46:02 +0200

    first commit

---
+++ Assets/DragonControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Animation>().Play("Default Take");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Animation>().Play("walk");
+        } /*else
+        {
+            GetComponent<Animation>().Play("Default Take");
+        }*/
+    }
+}
